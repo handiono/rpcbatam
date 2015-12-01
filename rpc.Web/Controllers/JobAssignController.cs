114c8f9bc026f9092c1a3dfaa84dd0c4e820b7e1
rjuzariah <riajuzariah@gmail.com>
2015-12-02 05:20:52 +0700

    Drag

---
+++ rpc.Web/Controllers/JobAssignController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rpc.Web.Controllers
+{
+    public class JobAssignController : Controller
+    {
+        // GET: JobAssign
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
