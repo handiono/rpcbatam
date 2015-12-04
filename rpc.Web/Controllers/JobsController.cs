649be2d10f7cae763a7c0dd304c742b29211075c
dwiariyanti <dwiariyanti121@gmail.com>
2015-12-04 15:39:28 +0700

    Add job and work center

--- rpc.Web/Controllers/JobsController.cs
+++ rpc.Web/Controllers/JobsController.cs
@@ -18,11 +18,7 @@ namespace rpc.Web.Controllers
         public ActionResult Index()
         {
             return View(db.Jobs.ToList());
-        
         }
-
-        
-
 
         // GET: Jobs/Details/5
         public ActionResult Details(string id)
@@ -61,27 +57,13 @@ namespace rpc.Web.Controllers
 
             return View(job);
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "JobID,WorkID,WorkName")] Work work)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Works.Add(work);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(work);
-        }
 
         // GET: Jobs/Edit/5
         public ActionResult Edit(string id)
         {
-            string query = "SELECT * FROM WORKS WHERE JobID= '"+ id +"'";
+            string query = "SELECT * FROM WORKS WHERE JobID= '" + id + "'";
             IEnumerable<Work> data = db.Database.SqlQuery<Work>(query);
             ViewBag.query = data.ToList();
-           
 
             if (id == null)
             {
@@ -93,23 +75,6 @@ namespace rpc.Web.Controllers
                 return HttpNotFound();
             }
             return View(job);
-
-            
-
-        }
-
-        public ActionResult EditWork(string id)
-        {
-             if ( id == null)
-             {
-                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-             }
-             Work work = db.Works.Find(id);
-            if (work == null)
-            {
-                return HttpNotFound();
-            }
-            return View(work);
         }
 
         // POST: Jobs/Edit/5
@@ -143,20 +108,6 @@ namespace rpc.Web.Controllers
             return View(job);
         }
 
-        public ActionResult DeleteWorkCenter(string id)
-        {
-            if ( id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Work work = db.Works.Find(id);
-            if( work == null)
-            {
-                return HttpNotFound();
-            }
-            return View(work);
-        }
-
         // POST: Jobs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -168,6 +119,67 @@ namespace rpc.Web.Controllers
             return RedirectToAction("Index");
         }
 
+
+
+        // POST: Works/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Creatework")]
+        public ActionResult Creatework([Bind(Include = "WorkID,WorkName,JobID")] Work work)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Works.Add(work);
+                db.SaveChanges();
+                
+            }
+
+            ViewBag.JobID = new SelectList(db.Jobs, "JobID", "JobName", work.JobID);
+            return RedirectToAction("Index");
+        }
+        // GET: Jobs/Edit/5
+
+        public ActionResult EditWork(string id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Work work = db.Works.Find(id);
+            if (work == null)
+            {
+                return HttpNotFound();
+            }
+            return View(work);
+        }
+
+        // POST: Work/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditWork([Bind(Include = "JobID,WorkID,WorkName")] Work work)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(work).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(work);
+        }
+        // POST: Work/Delete/5
+        [HttpPost, ActionName("DeleteWork")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteWork(string id)
+        {
+            Work work = db.Works.Find(id);
+            db.Works.Remove(work);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+      
         protected override void Dispose(bool disposing)
         {
             if (disposing)
